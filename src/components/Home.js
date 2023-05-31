20c75c32579d9339eb69b517087b1fb0f6213448
Carlos Garcia <carlos.garcia@boatyardx.com>
2023-05-31 12:10:51 -0500

    Add home route

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,26 @@
 import React from "react";
+import useTeamNames from "../hooks/useTeamNames";
+import { Link } from "react-router-dom";
+import TeamLogo from "./TeamLogo";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { response, loading } = useTeamNames();
+
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <div className="container">
+      <h1 className="large-header">Hash History Basketball League</h1>
+      <h3 className="header text-center">Select a team</h3>
+      <div className="home-grid">
+        {response.map((team) => (
+          <Link to={`${team}`}>
+            <TeamLogo id={team} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
