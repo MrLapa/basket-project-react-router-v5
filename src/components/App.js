3b1bf50ca3a66aacafd97b4aab1549519b554215
Carlos Garcia <carlos.garcia@boatyardx.com>
2023-05-31 11:35:36 -0500

    Catch all routes 404

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,30 @@
 import * as React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Players from "./Players";
 import Teams from "./Teams";
 import Home from "./Home";
 import Navbar from "./Navbar";
+import NotFound from "./NotFound";
 
 export default function App() {
   return (
     <Router>
       <div>
         <Navbar />
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/players">
-          <Players />
-        </Route>
-        <Route path="/teams">
-          <Teams />
-        </Route>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/players">
+            <Players />
+          </Route>
+          <Route path="/teams">
+            <Teams />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
