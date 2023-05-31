965585aef6ae6a36b793e9154ad04d84175341a2
Carlos Garcia <carlos.garcia@boatyardx.com>
2023-05-31 11:14:25 -0500

    Create initial routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,25 @@
-import * as React from 'react'
+import * as React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Players from "./Players";
+import Teams from "./Teams";
+import Home from "./Home";
+import Navbar from "./Navbar";
 
-export default function App () {
+export default function App() {
   return (
-    <div>
-      Hash History Basketball League
-    </div>
-  )
+    <Router>
+      <div>
+        <Navbar />
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/players">
+          <Players />
+        </Route>
+        <Route path="/teams">
+          <Teams />
+        </Route>
+      </div>
+    </Router>
+  );
 }
